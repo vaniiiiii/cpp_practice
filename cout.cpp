29bd38710b664cf88c9b7fd78136cd1ddb8587e4
Vani Kumar <94632302+vaniiiiii@users.noreply.github.com>
2022-11-29 14:07:02 +0530

    Add files via upload

---
+++ cout.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+ long long int b;
+ char c;
+ float d;
+ double e;
+
+ cin>>a>>b>>c>>d>>e;
+
+ printf("%d\n%lld\n%c\n%f\n%lf",a,b,c,d,e);
+
+return 0;
+}
